68218b889d112609190d37e5dde5d2192d58e2b2
Mack <56776327+psmack@users.noreply.github.com>
2023-02-28 19:58:36 -1000

    setup gh-pages

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-// next.config.js
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: './public',
-  },
-}
-
+module.exports = nextConfig
