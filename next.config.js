48e32fd629da31abbe7fb7dab5f9bb61ea9c4408
Mack <56776327+psmack@users.noreply.github.com>
2023-02-28 18:50:19 -1000

    Update config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/**
+   * @type {import('next').NextConfig}
+   */
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
-module.exports = nextConfig
+export default   nextConfig;
+
