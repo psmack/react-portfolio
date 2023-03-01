5ac2eb6e55f3972dae9247d9cde7e20125b8f550
Mack <56776327+psmack@users.noreply.github.com>
2023-02-28 20:35:26 -1000

    Revert next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/psmack/react-portfolio/gh-pages/' : '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
