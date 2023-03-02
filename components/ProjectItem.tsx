7c33c6f909e3a33ba52792cb32aff668808f8f65
Mack <56776327+psmack@users.noreply.github.com>
2023-03-01 18:45:08 -1000

    Created a component for repo card

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiFillGithub } from 'react-icons/ai'
 
-const ProjectItem = () => {
+const ProjectItem = ({ title, description, url }) => {
   return (
-    <div>ProjectItem</div>
+    <div className='relative'>
+      <div className='repo-card'>
+        <div className='repo-text-box'>
+          <h4 className='repo-title'>{title}</h4>
+          <p>{description}</p>
+          </div> 
+          <Link href={url} className='github-box'>
+            <AiFillGithub className='github-icon'/>
+          </Link>
+        </div>
+    </div>
   )
 }
 
